2a343f2123834a168857aaf87361054f65577ffa
gazzonn <demondarklite@gmail.com>
2024-12-11 16:03:01 +0300

    Translated an application to Russian.

--- ToDoList/ToDoList/Models/Delo.cs
+++ ToDoList/ToDoList/Models/Delo.cs
@@ -9,17 +9,22 @@ namespace ToDoList.Models
 
         [Required]
         [StringLength(100)]
+        [Display(Name = "Название")]
         public string Title { get; set; }
 
         [DataType(DataType.MultilineText)]
+        [Display(Name = "Описание")]
         public string Description { get; set; }
 
         [DataType(DataType.Date)]
+        [Display(Name = "Дата создания")]
         public DateTime CreatedDate { get; set; }
 
         [DataType(DataType.Date)]
+        [Display(Name = "Срок выполнения")]
         public DateTime DueDate { get; set; }
-
+        
+        [Display(Name = "Выполнение")]
         public bool IsCompleted { get; set; }
     }
 }
